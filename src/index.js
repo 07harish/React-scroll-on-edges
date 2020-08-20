1ea5041ca2376a4de535b31ef5489afec2db43c4
Harish Kulkarni <07harish96@gmail.com>
2020-08-21 01:18:58 +0530

    (refactor) files

--- src/index.js
+++ src/index.js
@@ -1,5 +1,1 @@
-import React from 'react'
-import styles from './styles.module.css'
-// import useScrollOnEdges  from './hooks/useScrollOnEdges'
-
-export {default} from './hooks/useScrollOnEdges'
+export { default } from './hooks/useScrollOnEdges'
